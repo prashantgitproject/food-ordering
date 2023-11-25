86cfc9216725feedb36e96d008562d8824c0e754
Prashant SIngh <prashant.sb4754@gmail.com>
2023-11-25 19:25:58 +0530

    navbar component completed

---
+++ src/components/layout/Header.js
@@ -0,0 +1,20 @@
+import Link from "next/link"
+
+export default function Header() {
+  return (
+    <>
+        <header className="flex items-center justify-between">
+        <Link className='text-primary font-semibold text-2xl' href="">
+          ST PIZZA
+        </Link>
+        <nav className="flex gap-8 items-center text-gray-500 font-semibold">
+          <Link href={''}>Home</Link>
+          <Link href={''}>Menu</Link>
+          <Link href={''}>Contact</Link>
+          <Link href={''}>About</Link>
+          <Link className="bg-primary text-white px-8 py-2 rounded-full" href={''}>Login</Link>
+        </nav>
+      </header> 
+    </>
+  )
+}
