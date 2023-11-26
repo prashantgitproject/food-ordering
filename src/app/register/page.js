5d8bca5269726c85448679d1ce0d2f2e2d322073
Prashant SIngh <prashant.sb4754@gmail.com>
2023-11-26 20:11:43 +0530

    registration page, hashing, adding user to database

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,15 +1,54 @@
+"use client"
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function RegisterPage() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('');
+    const [creatingUser, setCreatingUser] = useState(false)
+    const [userCreated, setUserCreated] = useState(false)
+    const [error, setError] = useState(false)
+    async function handleFormSubmit(ev){
+        ev.preventDefault();
+        setCreatingUser(true);
+        setError(false)
+        setUserCreated(false)
+        const response = await fetch("/api/register", {
+            method: 'POST',
+            body: JSON.stringify({email, password}),
+            headers: {'Content-Type': 'application/json'}
+        });
+        if(response.ok){
+            setUserCreated(true)  
+        }
+        else{
+            setError(true)
+        }
+        setCreatingUser(false)
+        
+        
+    }
   return (
     <section className="mt-8">
         <h1 className="text-primary text-4xl text-center mb-4">
             Register
         </h1>
-        <form className="block max-w-xs mx-auto">
-            <input type="email" placeholder="email"/>
-            <input type="password" placeholder="password"/>
-            <button type="submit">Register</button>
+        {userCreated && (
+            <div className="my-4 text-center">
+                User created.<br/> Now you can {' '} <Link className="underline" href={"/"}>Login &raquo;</Link>
+            </div>
+        )}
+        {error && (
+            <div className="my-4 text-center">
+                An error has occured.<br/>
+                Please try again later
+            </div>
+        )}
+        <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+            <input type="email" placeholder="email" value={email} onChange={ev => setEmail(ev.target.value)} disabled={creatingUser}/>
+            <input type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)} disabled={creatingUser}/>
+            <button type="submit" disabled={creatingUser}>Register</button>
             <div className="text-center text-gray-500 my-4">
                 Or login with provider
             </div>
---
+++ src/models/User.js
@@ -0,0 +1,26 @@
+import bcrypt from 'bcrypt'
+const { Schema, models, model } = require("mongoose");
+
+const UserSchema = new Schema({
+    email: {type: String, required: true, unique: true},
+    password: {
+        type: String,
+        required:true,
+        validate: pass => {
+            if(!pass?.length || pass.length < 5){
+                new Error('password must be of atleast 5 characters')
+                return false
+            }
+        }
+        }
+    }, {timestamps: true})
+
+
+    UserSchema.post('validate', function (user){
+        const notHashedPassword = user.password
+        const salt = bcrypt.genSaltSync(10);
+        user.password = bcrypt.hashSync(notHashedPassword, salt);
+    })
+
+export const User = models?.User || model('User', UserSchema)
+
