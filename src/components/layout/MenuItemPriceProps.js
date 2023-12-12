775fc02f2208089d69c5ebff111037487cea87e6
Prashant SIngh <prashant.sb4754@gmail.com>
2023-12-12 11:21:33 +0530

    building menu-item page MenuItemPriceProps completed

--- src/components/layout/MenuItemPriceProps.js
+++ src/components/layout/MenuItemPriceProps.js
@@ -1,7 +1,12 @@
 import Trash from '@/components/icons/Trash'
 import Plus from '@/components/icons/Plus'
+import ChevronDown from '@/components/icons/ChevronDown'
+import ChevronUp from '@/components/icons/ChevronUp'
+import { useState } from 'react';
 
 export default function MenuItemPriceProps({name, addLabel, props, setProps}) {
+
+    const [isOpen, setIsOpen] = useState(false)
 
     function addProp() {
         setProps(oldProps => {
@@ -26,31 +31,43 @@ export default function MenuItemPriceProps({name, addLabel, props, setProps}) {
 
   return (
     <div className="bg-gray-200 p-2 rounded-md mb-2">
-        <label>{name}</label>
-        {props?.length > 0 && props.map((size, index) => (
-            <div className="flex items-end gap-2">   
-                <div>
-                    <label>Name</label>
-                    <input type="text" placeholder="Size name"
-                        value={size.name} onChange={ev => {editProp(ev, index, 'name')}}/>
-                </div>
-                <div>
-                    <label>Extra price</label>
-                    <input type="text" placeholder="Extra price"
-                        value={size.price} onChange={ev => {editProp(ev, index, 'price')}}/>
-                </div>
-                <div>
-                    <button onClick={() => removeProp(index)}
-                        type="button" className="bg-white mb-2 px-2"><Trash/></button>
-                </div>
-            </div>
-        ))}
-        <button type="button"
-            onClick={addProp}
-            className="bg-white items-center">
-            <Plus className="w-4 h-4"/>
-            <span>{addLabel}</span>
+        <button className='inline-flex p-1 border-0 justify-start'
+        onClick={() => setIsOpen(prev => !prev)} type='button'>
+        {isOpen && (
+            <ChevronUp/>
+        )}
+        {!isOpen && (
+            <ChevronDown/>
+        )}
+            <span>{name}</span>
+            <span>({props?.length})</span>
         </button>
+        <div className={isOpen ? 'block' : 'hidden'}>
+            {props?.length > 0 && props.map((size, index) => (
+                <div className="flex items-end gap-2">   
+                    <div>
+                        <label>Name</label>
+                        <input type="text" placeholder="Size name"
+                            value={size.name} onChange={ev => {editProp(ev, index, 'name')}}/>
+                    </div>
+                    <div>
+                        <label>Extra price</label>
+                        <input type="text" placeholder="Extra price"
+                            value={size.price} onChange={ev => {editProp(ev, index, 'price')}}/>
+                    </div>
+                    <div>
+                        <button onClick={() => removeProp(index)}
+                            type="button" className="bg-white mb-2 px-2"><Trash/></button>
+                    </div>
+                </div>
+            ))}
+            <button type="button"
+                onClick={addProp}
+                className="bg-white items-center">
+                <Plus className="w-4 h-4"/>
+                <span>{addLabel}</span>
+            </button>
+        </div>
     </div>
   )
 }
