f8f1815af96d3473719242da6fd7f23ee178e5d3
Prashant SIngh <prashant.sb4754@gmail.com>
2024-01-08 21:13:09 +0530

    order now button link

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Right from "../icons/Right";
+import Link from "next/link";
 
 export default function Hero() {
   return (
@@ -9,8 +10,10 @@ export default function Hero() {
             <p className="my-6 text-gray-500 text-sm">Pizza is a missing piece that makes everyday complete, a simple yet delicious joy in life</p>
             <div className="flex gap-4 text-sm">
                 <button className="flex justify-center bg-primary uppercase items-center gap-2 text-white px-4 py-2 rounded-full">
-                    Order Now
-                    <Right/>
+                    <Link href={'/menu'} className="flex justify-center gap-2 items-center">
+                        Order Now
+                        <Right/>
+                    </Link>
                 </button>
                 <button className="items-center flex border-0 gap-2 py-2 text-gray-600 font-semibold">
                     Learn More
